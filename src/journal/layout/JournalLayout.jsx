3646bc7f5196763514bc32fc6698174708467f63
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-07-15 22:58:38 -0500

    agregando navbar al journal page

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,24 @@
-export const JournalLayout = () => {
+import { Box } from "@mui/material"
+import { NavBar } from "../components";
+
+const drawerWidth = 240;
+
+export const JournalLayout = ( { children } ) => {
   return (
-    <div>JournalLayout</div>
+    <Box sx={{display: 'flex'}}>
+
+      {/* Navbar */}
+      <NavBar drawerWidth={ drawerWidth } />
+
+      {/* SideBar */}
+
+      <Box component='main' sx={{flexGrow: 1, p: 3}} >
+
+        {/* Toolbar */}
+
+        { children }
+      </Box>
+
+    </Box>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,16 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1" >JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, voluptas vitae. Ut perferendis quia vel explicabo facere amet ducimus cum nam pariatur animi culpa consectetur ullam, autem vero perspiciatis adipisci dolorem officia itaque. Expedita voluptatum at ea. In corrupti eum animi ullam delectus quaerat ipsum, minus possimus? Odio, distinctio accusantium.</Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+
+    </JournalLayout>
   )
 }
